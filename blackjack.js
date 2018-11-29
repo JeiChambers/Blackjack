96680e22884dfe4f96b199e8a888b098d41a8a48
JeiChambers <jeilee88@outlook.com>
2018-11-28 20:09:33 -0600

    Initial Commit

---
+++ blackjack.js
@@ -0,0 +1,23 @@
+
+//Blackjack 21 
+
+//By Jei Chambers
+
+let suits = ["Spades", "Clubs", "Hearts", "Diamond"];
+let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", 
+	"Seven", "Six", "Five", "Four", "Three", "Two",];
+
+let deck = [];
+
+for (let suitIdx = 0; suitIdx < suits.length; suitIdx++;){
+	for(let valueIdx = 0; valueIdx < value.length; valueIdx++){
+		deck.push(values[valueIdx] + " of " suits[suitIdx]);
+	}
+
+}
+
+for (let i = 0; i < deck.length; i++;){
+
+}
+
+let playerCards = [deck[0], deck[2]];
